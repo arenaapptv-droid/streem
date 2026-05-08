8811b2495d8eece871f84fad96b192c7f44a1868
RodoYd <arenaapptv@gmail.com>
2026-05-09 01:55:40 +0300

    Create live.php

---
+++ public/live.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://prod-fastly-eu-west-3.video.pscp.tv/Transcoding/v1/hls/FvBy6bSkfG9YDVmeU1nDx4hTrB0mpiVSwYUlBtzsQlNgBm2G8kuoXBvu9a_EXoRyhUE-SyH-gBxT_jLg9N4Ehw/non_transcode/eu-west-3/periscope-replay-direct-prod-eu-west-3-public/master_dynamic_highlatency.m3u8?type=live");
+exit;
+?>
